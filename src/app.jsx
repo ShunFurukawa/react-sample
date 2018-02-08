fc4ae330fd0fa3a8090a57b67c7c3b786ac49b89
Shun Furukawa <furukawa9312@gmail.com>
2018-02-08 21:31:47 +0900

    :octocat: fix dir dist index

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Form from './components/form'
+import Message from './components/message'
+
 ReactDOM.render(
-  <div>Hello, React~!!!</div>,
+  <div>
+    Hello, React~!!!
+    <Form />
+    <Message />
+  </div>,
   document.getElementById('root')
 )
---
+++ src/components/form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Form extends React.Component {
+  render() {
+    return(
+      <div>
+        <form>
+          <input type="text" />
+          <button type="submit">送信~~!!</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form
