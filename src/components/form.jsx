12028f38c6ce36a5e35f62a1f54b417762ac39b2
Shun Furukawa <furukawa9312@gmail.com>
2018-02-09 01:52:07 +0900

    :octocat: modify form jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 
 class Form extends React.Component {
+
+  constructor() {
+    super()
+      this.state = {
+        message: '',
+        data: [],
+      }
+
+  this.changeText = this.changeText.bind(this)
+  this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event) {
+    evennt.preventDefault()
+    this.setSate({data: this.state.data.concat(this.state.message)})
+  }
+
+  changeText(event) {
+    this.setSate({message: event.target.value})
+  }
+
   render() {
     return(
       <div>
         <form>
-          <input type="text" />
-          <button type="submit">送信~~!!</button>
+          <input onChange={this.changeText} type="text" />
+          <button onClick={this.handleClick}>
+            送信~~!!
+          </button>
+          {this.state.data.map((message, idx) => <Message message={message} key={idx} />)}
         </form>
       </div>
     )
